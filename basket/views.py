d40009e7c78d9f3caa9622d6bb74f41b97d79cb4
CamCove-Crichton <>
2023-10-02 02:59:37 +0000

    Third attempt at reverting to previous commit

--- basket/views.py
+++ basket/views.py
@@ -26,23 +26,23 @@ def add_to_basket(request, item_id):
         print(booking_form.errors)
         print(booking_form)
 
-    number_of_attendees = int(request.POST.get('number_of_attendees'))
-    redirect_url = request.POST.get('redirect_url')
-    basket = request.session.get('basket', {})
+        number_of_attendees = int(request.POST.get('number_of_attendees'))
+        redirect_url = request.POST.get('redirect_url')
+        basket = request.session.get('basket', {})
 
-    if item_id in list(basket.keys()):
-        basket[item_id] += number_of_attendees
-    else:
-        basket[item_id] = number_of_attendees
-
-            request.session['basket'] = basket
-            return redirect(redirect_url)
+        if item_id in list(basket.keys()):
+            basket[item_id] += number_of_attendees
         else:
-            context = {
-                'booking_form': booking_form,
-                'tour': tour,
-            }
+            basket[item_id] = number_of_attendees
 
-            template = 'tours/tour_detail.html'
+                request.session['basket'] = basket
+                return redirect(redirect_url)
+            else:
+                context = {
+                    'booking_form': booking_form,
+                    'tour': tour,
+                }
 
-            return render(request, template, context)
+                template = 'tours/tour_detail.html'
+
+                return render(request, template, context)
