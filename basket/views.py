9fc2301c5d7cf61447712861f76283316a71ee34
CamCove-Crichton <>
2023-10-02 02:36:10 +0000

    Attempt to revert back to previous commit

--- basket/views.py
+++ basket/views.py
@@ -26,43 +26,14 @@ def add_to_basket(request, item_id):
         print(booking_form.errors)
         print(booking_form)
 
-        if booking_form.is_valid():
-            number_of_attendees = (booking_form.cleaned_data
-                                   ['number_of_attendees'])
-            booking_date = booking_form.cleaned_data['booking_date']
-            booking_time_slot = booking_form.cleaned_data['booking_time_slot']
+    number_of_attendees = int(request.POST.get('number_of_attendees'))
+    redirect_url = request.POST.get('redirect_url')
+    basket = request.session.get('basket', {})
 
-            basket = request.session.get('basket', {})
-
-            if (isinstance(basket.get(item_id), dict) and
-                    'items_by_date_and_time' in basket[item_id]):
-                if booking_date in basket[item_id]['items_by_date_and_time']:
-                    # The tour for the same day exists in the basket
-                    if (booking_time_slot in basket[item_id]
-                            ['items_by_date_and_time'][booking_date]):
-                        # Tour for same date and time slot already in basket
-                        (basket[item_id]['items_by_date_and_time']
-                            [booking_date]
-                            [booking_time_slot]) += number_of_attendees
-                    else:
-                        # Tour with same date exists with different time slot
-                        (basket[item_id]['items_by_date_and_time']
-                            [booking_date]
-                            [booking_time_slot]) = number_of_attendees
-                else:
-                    # The tour for a different date needs to be added
-                    basket[item_id]['items_by_date_and_time'][booking_date] = {
-                        booking_time_slot: number_of_attendees
-                    }
-            else:
-                # Tour not in the basket so add it with specified date and time
-                basket[item_id] = {
-                    'items_by_date_and_time': {
-                        booking_date: {
-                            booking_time_slot: number_of_attendees
-                        }
-                    }
-                }
+    if item_id in list(basket.keys()):
+        basket[item_id] += number_of_attendees
+    else:
+        basket[item_id] = number_of_attendees
 
             request.session['basket'] = basket
             return redirect(redirect_url)
